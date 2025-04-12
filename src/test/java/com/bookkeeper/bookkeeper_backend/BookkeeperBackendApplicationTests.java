be04f01b9b9343d1bda243ba553c4126a3bbd830
Ianne <mariannesantos174@gmail.com>
2025-04-12 17:18:48 +0800

    Initializing the remote repository.

---
+++ src/test/java/com/bookkeeper/bookkeeper_backend/BookkeeperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookkeeper.bookkeeper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeeperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
